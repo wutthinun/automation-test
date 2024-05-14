ea79c68a8e0127181a6b56145483a7448b87af00
Chonnabot Duangprasit <chonnabot.dua@cjexpress.co.th>
2024-05-14 18:47:28 +0700

    fix id

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,19 +1,19 @@
 import React from "react";
-import {Button, Form, Input,} from "antd";
-import {Link, useNavigate} from "react-router-dom";
+import { Button, Form, Input } from "antd";
+import { Link, useNavigate } from "react-router-dom";
 
 import "./login.style.css";
 import logo from "src/assets/cj-logo-i.png";
 type FormValueType = {
-  userName: string
-  password: string
-}
+  userName: string;
+  password: string;
+};
 function LoginPage() {
   const navigate = useNavigate();
   const [form] = Form.useForm();
-  const account = {userName: "admin", password: "password"};
+  const account = { userName: "admin", password: "password" };
 
-  const onFinish = (values:FormValueType) => {
+  const onFinish = (values: FormValueType) => {
     console.log("Success:", values);
     if (
       values.userName === account.userName &&
@@ -33,7 +33,7 @@ function LoginPage() {
       ]);
     }
   };
-  const onFinishFailed = (errorInfo:any) => {
+  const onFinishFailed = (errorInfo: any) => {
     console.log("Failed:", errorInfo);
   };
 
@@ -42,12 +42,10 @@ function LoginPage() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <b>
-            CJ More System
-          </b>
+          <b>CJ More System</b>
         </header>
       </div>
-     
+
       <div className="form-container ">
         <Form
           form={form}
@@ -70,7 +68,7 @@ function LoginPage() {
               },
             ]}
           >
-            <Input id="username" test-id="username" size="large"/>
+            <Input id="username" test-id="username" size="large" />
           </Form.Item>
 
           <Form.Item
@@ -83,18 +81,31 @@ function LoginPage() {
               },
             ]}
           >
-            <Input.Password  id="password" test-id="password" size="large"/>
+            <Input.Password id="password" test-id="password" size="large" />
           </Form.Item>
 
           <Form.Item>
-            <Button className="btn-submit" type="primary" htmlType="submit" size="large" id="btn-submit" test-id="btn-submit">
+            <Button
+              className="btn-submit"
+              type="primary"
+              htmlType="submit"
+              size="large"
+              id="btn-submit"
+              test-id="btn-submit"
+            >
               Submit
             </Button>
           </Form.Item>
           <div className="register">
-            <Link id="hpl-register" test-id="hpl-register" to='/register' style={{color:"#019c50"}}>Click for register</Link>
+            <Link
+              id="hpl-register"
+              test-id="hpl-register"
+              to="/register"
+              style={{ color: "#019c50" }}
+            >
+              Click for register
+            </Link>
           </div>
-          
         </Form>
       </div>
     </div>
--- src/pages/register/RegisterForm.tsx
+++ src/pages/register/RegisterForm.tsx
@@ -4,131 +4,158 @@ import Form, { FormProps } from "antd/es/form";
 import Input from "antd/es/input";
 import { useNavigate } from "react-router-dom";
 
-import './register.style.css'
+import "./register.style.css";
 import { FieldType } from "../../model";
 
 function RegisterForm() {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
-    
-    useEffect(() => {
-        localStorage.removeItem('data');
-    }, []);
+  useEffect(() => {
+    localStorage.removeItem("data");
+  }, []);
 
-    const onFinish: FormProps<FieldType>['onFinish'] = (values: FieldType) => {
-        localStorage.setItem('data', JSON.stringify(values));
-        navigate('/register/result');
-    };
+  const onFinish: FormProps<FieldType>["onFinish"] = (values: FieldType) => {
+    localStorage.setItem("data", JSON.stringify(values));
+    navigate("/register/result");
+  };
 
-    const onFinishFailed: FormProps<FieldType>['onFinishFailed'] = (errorInfo: any) => {
-        console.log('Failed:', errorInfo);
-    };
+  const onFinishFailed: FormProps<FieldType>["onFinishFailed"] = (
+    errorInfo: any
+  ) => {
+    console.log("Failed:", errorInfo);
+  };
 
-    return (
-        <div className="register-container">
-			<Form
-				name="register"
-				layout="vertical"
-				style={{ maxWidth: 600 }}
-				initialValues={{ remember: true }}
-				onFinish={onFinish}
-				onFinishFailed={onFinishFailed}
-				autoComplete="off"
-			>
-				<Form.Item<FieldType>
-					label="User name"
-					name="username"
-					rules={[{ required: true, message: 'Please input your first name!' }]}
-				>
-					<Input id="username" test-id="username" size="large"/>
-				</Form.Item>
+  return (
+    <div className="register-container">
+      <Form
+        name="register"
+        layout="vertical"
+        style={{ maxWidth: 600 }}
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        <Form.Item<FieldType>
+          label="User name"
+          name="username"
+          rules={[{ required: true, message: "Please input your username!" }]}
+        >
+          <Input id="username" test-id="username" size="large" />
+        </Form.Item>
 
-				<Form.Item<FieldType>
-					label="Password"
-					name="password"
-					rules={[{ required: true, message: 'Please input your first name!' }]}
-				>
-					<Input.Password id="password" test-id="password" size="large"/>
-				</Form.Item>
+        <Form.Item<FieldType>
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: "Please input your password!" }]}
+        >
+          <Input.Password id="password" test-id="password" size="large" />
+        </Form.Item>
 
-				<Form.Item<FieldType>
-					label="First name"
-					name="firstname"
-					rules={[{ required: true, message: 'Please input your first name!' }]}
-				>
-					<Input id="firstname" test-id="firstname" size="large"/>
-				</Form.Item>
+        <Form.Item<FieldType>
+          label="First name"
+          name="firstname"
+          rules={[{ required: true, message: "Please input your first name!" }]}
+        >
+          <Input id="firstname" test-id="firstname" size="large" />
+        </Form.Item>
 
-				<Form.Item<FieldType>
-					label="Last name"
-					name="lastname"
-					rules={[{ required: true, message: 'Please input your last name!' }]}
-				>
-					<Input id="lastname" test-id="lastname" size="large"/>
-				</Form.Item>
+        <Form.Item<FieldType>
+          label="Last name"
+          name="lastname"
+          rules={[{ required: true, message: "Please input your last name!" }]}
+        >
+          <Input id="lastname" test-id="lastname" size="large" />
+        </Form.Item>
 
-				<Form.Item<FieldType>
-					label="Gender"
-					name="gender"
-					rules={[{ required: true, message: 'Please input your gender!' }]}
-				>
-					<Radio.Group >
-						<Radio id="gender-male" test-id="gender-male" value="Male"style={{fontSize:'16px'}} > Male </Radio>
-						<Radio id="gender-female" test-id="gender-female" value="Female" style={{fontSize:'16px'}}> Female </Radio>
-					</Radio.Group>
-				</Form.Item>
+        <Form.Item<FieldType>
+          label="Gender"
+          name="gender"
+          rules={[{ required: true, message: "Please input your gender!" }]}
+        >
+          <Radio.Group>
+            <Radio
+              id="gender-male"
+              test-id="gender-male"
+              value="Male"
+              style={{ fontSize: "16px" }}
+            >
+              &nbsp;Male
+            </Radio>
+            <Radio
+              id="gender-female"
+              test-id="gender-female"
+              value="Female"
+              style={{ fontSize: "16px" }}
+            >
+              &nbsp;Female
+            </Radio>
+          </Radio.Group>
+        </Form.Item>
 
-				<Form.Item<FieldType>
-					label="Membership Type"
-					name="membershipType"
-					rules={[{ required: true, message: 'Please select your membership type!' }]}
-				>
-					<Select id="membership-type" test-id="membership-type" size="large" options={[
-						{
-							label: 'Silver',
-							value: 'Silver'
-						},
-						{
-							label: 'Gold',
-							value: 'Gold'
-						},
-						{
-							label: 'Premium',
-							value: 'Premium'
-						}
-					]}>
-					</Select>
-				</Form.Item>
+        <Form.Item<FieldType>
+          label="Membership Type"
+          name="membershipType"
+          rules={[
+            { required: true, message: "Please select your membership type!" },
+          ]}
+        >
+          <Select
+            id="membership-type"
+            test-id="membership-type"
+            size="large"
+            options={[
+              {
+                label: "Silver",
+                value: "Silver",
+              },
+              {
+                label: "Gold",
+                value: "Gold",
+              },
+              {
+                label: "Premium",
+                value: "Premium",
+              },
+            ]}
+          ></Select>
+        </Form.Item>
 
-				<Form.Item<FieldType>
-					name="tnc"
-					valuePropName="checked"
-					rules={[
-						{ validator(rule, value, callback) {
-							if (!value) {
-								callback('Please agree to the terms and conditions!');
-							} else {
-								callback();
-							}
-						},}
-					]}
-				>
-					<Checkbox id="terms-cons" test-id="terms-cons">I agree to the terms and conditions</Checkbox>
-				</Form.Item>
+        <Form.Item<FieldType>
+          name="tnc"
+          valuePropName="checked"
+          rules={[
+            {
+              validator(rule, value, callback) {
+                if (!value) {
+                  callback("Please agree to the terms and conditions!");
+                } else {
+                  callback();
+                }
+              },
+            },
+          ]}
+        >
+          <Checkbox id="terms-cons" test-id="terms-cons">
+            I agree to the terms and conditions
+          </Checkbox>
+        </Form.Item>
 
-				<Form.Item >
-					<Button type="primary" htmlType="submit"
-					id="register-submit"
-					test-id="register-submit"
-					style={{width:'100%'}}
-					size="large"
-					>
-						Submit
-					</Button>
-				</Form.Item>
-			</Form>
-        </div>
-    )
+        <Form.Item>
+          <Button
+            type="primary"
+            htmlType="submit"
+            id="register-submit"
+            test-id="register-submit"
+            style={{ width: "100%" }}
+            size="large"
+          >
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
 }
 
 export default RegisterForm;
